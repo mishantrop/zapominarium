05d6d33fbc7e66ad19ba3694257cb1531b11b2e7
mikhails <ultimatuleev@gmail.com>
2020-05-01 15:27:03 +0400

    Change complexity

--- app/src/main/java/com/example/myapplication/Field.kt
+++ app/src/main/java/com/example/myapplication/Field.kt
@@ -7,97 +7,159 @@ import android.widget.Toast
 import androidx.gridlayout.widget.GridLayout
 
 class Field {
-    private var enabled = true
+    private var isEnabled = true
     private var grid: GridLayout? = null
     private var successToast: Toast? = null
     private var width: Int? = null
     private var height: Int? = null
+    private var maxWidth = 5
+    private var maxHeight = 8
+    private val matchTargetCount = 2
+    private var context: Context? = null
     private val cells = mutableMapOf<Int, Cell>()
     private val emojies = arrayOf(
         "\uD83D\uDC7D", // 👽 Alien
         "\uD83D\uDC3B", // 🐻 Bear
+        "\uD83D\uDCA3", // 💣 Bomb
         "\uD83D\uDC31", // 🐱 Cat
-        "\uD83D\uDC14", // 🐔 Chicken
         "\uD83D\uDC36", // 🐶 Dog
         "\uD83D\uDC2C", // 🐬 Dolphin
         "\uD83D\uDC32", // 🐲 Dragon
-        "\uD83C\uDF83", // 🎃 Jack-O-Lantern
+        "\uD83D\uDD25", // 🔥 Fire
+        "\uD83D\uDC8E", // 💎 Gem Stone
         "\uD83C\uDF1F", // 🌟 Glowing Star
-        "\uD83C\uDF3A", // 🌺 Hibiscus
         "\uD83D\uDC1D", // 🐝 Honeybee
-        "\uD83D\uDC27",  // 🐧 Octopus
+        "\uD83C\uDF83", // 🎃 Jack-O-Lantern
+        "\uD83D\uDC27", // 🐧 Octopus
+        "\uD83D\uDC79", // 👹 Ogre
         "\uD83D\uDC3C", // 🐼 Panda
         "\uD83D\uDC37", // 🐷 Pig
         "\uD83C\uDF08", // 🌈 Rainbow
-        "\uD83D\uDC3A" // 🐺 Wolf
+        "\uD83C\uDF4E", // 🍎 Red Apple
+        "\uD83C\uDF85", // 🎅 Santa Claus
+        "\uD83C\uDF81", // 🎁 Wrapped Gift
+        "\uD83D\uDC3A"  // 🐺 Wolf
     )
 
-    constructor(width: Int, height: Int) {
-        val pairsCount = (width * height) / 2
+    public fun init(context: Context, grid: GridLayout, width: Int, height: Int) {
+        this.context = context
+        val pairsCount = (width * height) / this.matchTargetCount
         if (pairsCount > this.emojies.size) {
             throw Exception("I do not have emojies enough (${this.emojies.size}) to create $pairsCount pairs")
         }
-        if (width % 2 > 0 && height % 2 > 0) {
+        if (width % this.matchTargetCount > 0 && height % this.matchTargetCount > 0) {
             throw Exception("Count of cells must be even")
         }
+        if (width > this.maxWidth || height > this.maxHeight) {
+            throw Exception("Maximum size: ${this.maxWidth}x${this.maxHeight}")
+        }
+
 
         this.width = width
         this.height = height
+
+        this.successToast = Toast.makeText(this.context!!, "Ты молодец \uD83C\uDF1A", Toast.LENGTH_LONG)
+        this.grid = grid
+
+        this.reset()
+
+        this.grid?.columnCount = this.width!!
+        this.grid?.rowCount = this.height!!
+
+        this.render()
+    }
+
+    public fun decreaseComplexity() {
+        var width = this.width!!
+        var height = this.height!!
+
+        if (width % 2 != 0) {
+            width--
+        } else if (height % 2 != 0) {
+            height--
+        } else if (height > width) {
+            height--
+        } else {
+            width--
+        }
+
+        if (width <= 0 || height <= 0) {
+            return
+        }
+
+        this.init(this.context!!, this.grid!!, width, height)
+    }
+
+    public fun increaseComplexity() {
+        var width = this.width!!
+        var height = this.height!!
+
+        if (width % 2 != 0 && width < this.maxWidth) {
+            width++
+        } else if (height % 2 != 0 && height < this.maxHeight) {
+            height++
+        } else if (height > width) {
+            width++
+        } else {
+            height++
+        }
+
+        if (width > this.maxWidth || height > this.maxHeight) {
+            return
+        }
+
+        this.init(this.context!!, this.grid!!, width, height)
     }
 
     private fun getChars(pairsCount: Int): MutableList<String> {
         val chars = mutableListOf<String>()
-
         val pairsLastIndex = pairsCount - 1
-
         for (i in 0..pairsLastIndex) {
             chars.add(this.emojies[i])
             chars.add(this.emojies[i])
         }
-
         chars.shuffle()
-
         return chars
     }
 
-    fun init(context: Context, grid: GridLayout) {
+    private fun initCells() {
+        this.grid?.removeAllViews()
+        this.cells.clear()
+
         var cellId = 1;
-        this.successToast = Toast.makeText(context, "Ты молодец \uD83C\uDF1A", Toast.LENGTH_LONG)
-        this.grid = grid
 
         for (w in 1..this.width!!) {
             for (h in 1..this.height!!) {
-                var button = Button(context)
+                var button = Button(this.context)
                 val cell = Cell(
-                  cellId,
-                  button,
-                  onClick = { id -> this.onCellClick(id) }
+                    cellId,
+                    button,
+                    onClick = { id -> this.onCellClick(id) }
                 )
                 this.cells[cellId] = cell
                 this.grid?.addView(button)
                 cellId++
             }
         }
+
         this.initChars()
-        this.render()
     }
 
-    private fun onCellClick(id: Int) {
-        if (!this.enabled) {
+    private fun onCellClick(cellId: Int) {
+        if (!this.isEnabled) {
             return
         }
-        showCell(id)
-        val openedCells = this.cells.filter { (key, cell) -> cell.isVisible() && !cell.isMatched() }
+        showCell(cellId)
+        val openedCells = this.cells.filter { (_, cell) -> cell.isVisible() && !cell.isMatched() }
         val openedCellsChars = mutableSetOf<String>()
-        val matchTarget = 2
-        if (openedCells.size == matchTarget) {
-            for ((id, cell) in openedCells) {
+        if (openedCells.size == this.matchTargetCount) {
+            for ((_, cell) in openedCells) {
                 openedCellsChars.add(cell.getChar())
             }
 
             val isMatched = openedCellsChars.size == 1
             if (isMatched) {
-                for ((id, cell) in openedCells) {
+                for ((_, cell) in openedCells) {
                     cell.match()
                 }
             } else {
@@ -112,56 +174,52 @@ class Field {
             }
         }
 
-        val matchedCells = this.cells.filter { (key, cell) -> cell.isMatched() }
+        val matchedCells = this.cells.filter { (_, cell) -> cell.isMatched() }
         if (matchedCells.size == this.cells.size) {
             this.successToast?.show()
             this.reset()
+            this.render()
         }
     }
 
     private fun disable() {
-        this.enabled = false
+        this.isEnabled = false
     }
 
     private fun enable() {
-        this.enabled = true
+        this.isEnabled = true
     }
 
     private fun showCell(cellId: Int) {
         for ((id, cell) in this.cells) {
-            if (id === cellId) {
+            if (id == cellId) {
                 cell.show()
             }
         }
     }
 
     private fun reset() {
-        for ((id, cell) in this.cells) {
-            cell.reset()
-        }
-
-        this.initChars()
-        this.render()
+        this.initCells()
     }
 
     private fun initChars() {
         val chars = this.getChars((this.width!! * this.height!!) / 2)
         var charIndex = 0;
-        for ((id, cell) in this.cells) {
+        for ((_, cell) in this.cells) {
             cell.setChar(chars[charIndex])
             charIndex++
         }
     }
 
     private fun render() {
-        for ((id, cell) in this.cells) {
+        for ((_, cell) in this.cells) {
             cell.render()
         }
     }
 
     private fun hideUnmatchedCells() {
-        val unmatchedCells = this.cells.filter { (key, cell) -> !cell.isMatched() }
-        for ((id, cell) in unmatchedCells) {
+        val unmatchedCells = this.cells.filter { (_, cell) -> !cell.isMatched() }
+        for ((_, cell) in unmatchedCells) {
             cell.hide()
         }
     }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,24 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.gridlayout.widget.GridLayout
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    private var grid: GridLayout? = null
+    private var fieldGrid: GridLayout? = null
+    private var isDarkMode = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
-        val activityId = R.layout.activity_main
-        setContentView(activityId)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        this.grid = findViewById<GridLayout>(R.id.activity_main_grid)
+        setContentView(R.layout.activity_main)
+        this.fieldGrid = findViewById<GridLayout>(R.id.activity_main_grid)
+
+        var initialWidth = 5
+        var initialHeight = 6
+        val field = Field()
+        val buttonToggleTheme = findViewById<Button>(R.id.buttonToggleTheme)
+        val buttonDecreaseComplexity = findViewById<Button>(R.id.buttonDecreaseComplexity)
+        val buttonIncreaseComplexity = findViewById<Button>(R.id.buttonIncreaseComplexity)
+        buttonDecreaseComplexity.setOnClickListener {
+            field.decreaseComplexity()
+        }
+        buttonIncreaseComplexity.setOnClickListener {
+            field.increaseComplexity()
+        }
+
+        buttonToggleTheme.setOnClickListener {
+            this.isDarkMode = !this.isDarkMode
+            val mainLayout = findViewById<LinearLayout>(R.id.activity_main_layout)
+
+            if (this.isDarkMode) {
+                mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.backgroundDark))
+            } else {
+                mainLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.backgroundLight))
+            }
+        }
 
         try {
-            val width = 4
-            val height = 6
-            this.grid?.rowCount = height
-            this.grid?.columnCount = width
-            val field = Field(width, height)
-            field.init(this, this.grid!!)
+            this.fieldGrid?.columnCount = initialWidth
+            this.fieldGrid?.rowCount = initialHeight
+
+            field.init(this, this.fieldGrid!!, initialWidth, initialHeight)
         } catch (error: Exception) {
             val errorToast = Toast.makeText(this, error.message, Toast.LENGTH_LONG)
             errorToast.show()
